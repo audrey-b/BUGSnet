c6a3325d280f699199357c7da7e1592d0a0c6cbb
Justin Slater <40896411+JustinJSlater@users.noreply.github.com>
2018-12-05 17:20:04 -0500

    Added wrapper function for trace plots.

---
+++ R/nma.trace.R
@@ -0,0 +1,16 @@
+nma.trace <- function(jagsoutput, n="all"){
+  samples <- do.call(rbind, jagsoutput$samples) %>% data.frame() %>% select(starts_with("d."))
+  
+  if (n=="all"){
+    num_plots = ncol(samples)
+  } else {
+      num_plots = n
+  }
+
+  par(mfrow = c(num_plots, 2))
+  for (i in 1:num_plots){
+    plot(samples[,i], type="l", main = paste(names(samples[i])))
+    plot(density(samples[,i]), main = paste(names(samples[i])))
+  }
+   
+}
