a3f4e5e6cb4d7393b0b0e1169ef692883997cdd9
audrey-b <audrey476@hotmail.com>
2019-02-09 18:04:46 -0500

    missed one file in previous commit

--- R/nma.bugs.R
+++ R/nma.bugs.R
@@ -1,28 +1,28 @@
 #' Make Bugs Model
-#' @description Creates bugs BUGS code which will can be ran through \code{nma.analysis()}.
+#' @description Creates BUGS code which can be ran through \code{nma.run()}.
 #' 
-#' @param data.prep A data object produced by \code{data.prep()}
-#' @param outcome A string indicating the name of your outcome variable
+#' @param data A data object produced by \code{data.prep()}
+#' @param outcome A string indicating the name of your outcome variable.
 #' @param N A string indicating the name of the variable containing the number of participants in each arm
-#' @param reference A string for the treatment that will be seen as the 'referent' comparator and labeled as treatment 1 in the BUGS code. This is often
-#' a placebo or control drug of some kind.  
 #' @param sd A string (only required for continuous outcomes) indicating variable name
 #' of the standard deviation of the outcome
-#' @param type If type="inconsistency", an inconsistency model will be built. Otherwise a consistency model
-#' will be built
-#' @param exposure.time A string (only required for rate or rate2 outcomes) indicating the name of variable 
-#'   indicating person-time followup (e.g person years) or study followup time.
+#' @param reference A string for the treatment that will be seen as the 'referent' comparator and labeled as treatment 1 in the BUGS code. This is often
+#' a placebo or control drug of some kind.  
 #' @param family A string indicating the family of the distribution of the outcome. Options are:
 #' "binomial", "normal", "poisson".
 #' @param link The link function for the nma model. Options are "logit", "log", "cloglog", "identity".
+#' @param time A string (only required for binomial-cloglog or poisson-log models) indicating the name of variable 
+#'   indicating person-time followup (e.g person years) or study followup time.
 #' @param effects A string indicating the type of treatment effect relative to baseline. Options are "fixed" or "random".
-#' @param prior.mu A string of BUGS code to define priors on the baseline treatment effects. By default, independent normal priors are used with mean 0 and standard deviation 15u, where u is the largest maximum likelihood estimator in single trials (as suggested by ).
-#' @param prior.d A string of BUGS code to define priors on relative treatment effects. By default, independent normal priors are used with mean 0 and standard deviation 15u, where u is the largest maximum likelihood estimator in single trials (as suggested by ).
-#' @param prior.sigma A string of BUGS code for the priors on the variance of relative treatment effects. By default, a uniform distribution with range 0 to u is used, where u is the largest maximum likelihood estimator in single trials (as suggested by ).
-#' @param meta.covariate Optional string indicating the name of the variable in your data set that you would like to
-#' adjust for via meta regression.
+#' @param prior.mu A string of BUGS code that defines priors on the baseline treatment effects. By default, independent normal priors are used with mean 0 and standard deviation 15u, where u is the largest maximum likelihood estimator in single trials (as suggested by ).
+#' @param prior.d A string of BUGS code that defines define priors on relative treatment effects. By default, independent normal priors are used with mean 0 and standard deviation 15u, where u is the largest maximum likelihood estimator in single trials (as suggested by ).
+#' @param prior.sigma A string of BUGS code that defines the prior on the variance of relative treatment effects. By default, a uniform distribution with range 0 to u is used, where u is the largest maximum likelihood estimator in single trials (as suggested by ).
+#' @param covariate Optional string indicating the name of the variable in your data set that you would like to
+#' adjust for via meta regression. By default, covariate=NULL and no covariate adjustment is applied.
+#' @param type If type="inconsistency", an inconsistency model will be built. By default, type="consistency" and a consistency model is built.
+#' will be built.
 #' 
-#' @return \code{model.str} - A long character string containing BUGS code that will be run in \code{rjags}.
+#' @return \code{model} - A long character string containing BUGS code that will be run in \code{rjags}.
 #' @return \code{bugsdata2} - Data accompanying BUGS code.
 #' @return \code{scale} - The scale of the outcome, based on the chosen family and link function
 #' examples are "RR", "OR", "SMD", "HR"
@@ -35,7 +35,7 @@
 #' #Fixed effects, consistency model.
 #' #Binomial family, log link. This implies that the relative risk (RR) will be the scale.
 #' 
-#'nma.model(data.nma = my.slr,
+#'nma.model(data = my.slr,
 #'        outcome = "n_died", 
 #'        N = "n",
 #'        reference = "plbo",
@@ -47,7 +47,7 @@
 #' #Random Effects, inconsistency model
 #' #Normal family, identity link.        
 #'  
-#'nma.model(data.nma = my.slr,
+#'nma.model(data = my.slr,
 #'         outcome = "weight_change", 
 #'         N = "n",
 #'         reference = "control",
@@ -61,29 +61,29 @@
 #'# Random Effects, consistency model,
 #'# Poisson family, cloglog link.
 #'
-#'nma.model(data.nma = my.slr,
+#'nma.model(data = my.slr,
 #'         outcome = "n_died", 
 #'         N = "n",
 #'         reference = "plbo",
-#'         exposure.time="personyears",
+#'         time="personyears",
 #'         family = "poisson",
 #'         link = "cloglog",
 #'         effects = "random")
 
-nma.model <- function(data.nma,
+nma.model <- function(data,
                      outcome, 
                      N,
-                     reference,
-                     type=NULL,
                      sd=NULL,
-                     exposure.time=NULL,
+                     reference,
+                     type="consistency",
+                     time=NULL,
                      family,
                      link,
                      effects,
                      prior.mu = "DEFAULT",
                      prior.d = "DEFAULT",
                      prior.sigma = "DEFAULT",
-                     meta.covariate = NULL){
+                     covariate = NULL){
   if(link=="logit" & family %in% c("binomial", "binary", "bin", "binom")){
     scale <- "OR"
   }else if(link=="log" & family %in% c("binomial", "binary", "bin", "binom")){
@@ -96,19 +96,19 @@ nma.model <- function(data.nma,
     scale <- "HR"
   }
   
-  data <- data.nma$raw.data
+  data1 <- data$raw.data
   
   # rename variables as appropriate
-  names(data)[names(data) == data.nma$varname.t] <- "trt"
-  names(data)[names(data) == data.nma$varname.s] <- "trial"
-  names(data)[names(data) == outcome] <- "r1"
-  names(data)[names(data) == N] <- "N"
-  if (family == "normal"){names(data)[names(data) == sd] <- "sd"}
-  if (!is.null(exposure.time)){names(data)[names(data) == exposure.time] <- "timevar"}
-  if (!is.null(meta.covariate)){names(data)[names(data) == meta.covariate] <- "covariate"}
+  names(data1)[names(data1) == data$varname.t] <- "trt"
+  names(data1)[names(data1) == data$varname.s] <- "trial"
+  names(data1)[names(data1) == outcome] <- "r1"
+  names(data1)[names(data1) == N] <- "N"
+  if (family == "normal"){names(data1)[names(data1) == sd] <- "sd"}
+  if (!is.null(time)){names(data1)[names(data1) == time] <- "timevar"}
+  if (!is.null(covariate)){names(data1)[names(data1) == covariate] <- "covariate"}
   
   #Trt mapping
-  trt.map.table <- data$trt %>%
+  trt.map.table <- data1$trt %>%
     unique %>% 
     sort  %>%
     tibble(trt.ini=.) %>%
@@ -116,26 +116,26 @@ nma.model <- function(data.nma,
     add_row(trt.ini=reference, .before=1) %>%
     mutate(trt.jags = 1:dim(.)[1])
   
-  data %<>% mutate(trt.jags=mapvalues(trt,
+  data1 %<>% mutate(trt.jags=mapvalues(trt,
                                       from=trt.map.table$trt.ini,
                                       to=trt.map.table$trt.jags) %>% as.integer)
   
-  nt = data.nma$treatments %>% nrow()
-  ns = data.nma$studies %>% nrow()
-  na = data.nma$n.arms %>% select(n.arms) %>% t() %>% as.vector
+  nt = data$treatments %>% nrow()
+  ns = data$studies %>% nrow()
+  na = data$n.arms %>% select(n.arms) %>% t() %>% as.vector
   
   
   
-  sorted.data <- data 
+  sorted.data <- data1 
   
   # rename variables as appropriate
-  names(sorted.data)[names(sorted.data) == data.nma$varname.t] <- "trt"
-  names(sorted.data)[names(sorted.data) == data.nma$varname.s] <- "trial"
+  names(sorted.data)[names(sorted.data) == data$varname.t] <- "trt"
+  names(sorted.data)[names(sorted.data) == data$varname.s] <- "trial"
   names(sorted.data)[names(sorted.data) == outcome] <- "r1"
   names(sorted.data)[names(sorted.data) == N] <- "N"
   if (family == "normal"){names(sorted.data)[names(sorted.data) == sd] <- "sd"}
-  if (!is.null(exposure.time)){names(sorted.data)[names(sorted.data) == exposure.time] <- "timevar"}
-  if (!is.null(meta.covariate)){names(sorted.data)[names(sorted.data) == meta.covariate] <- "covariate"}
+  if (!is.null(time)){names(sorted.data)[names(sorted.data) == time] <- "timevar"}
+  if (!is.null(covariate)){names(sorted.data)[names(sorted.data) == covariate] <- "covariate"}
     
   sorted.data %<>% arrange(trial, trt.jags)
   
@@ -152,12 +152,12 @@ nma.model <- function(data.nma,
         r[i,a] <- sorted.data %>% select(r1) %>% slice(line+a-1) %>% as.numeric()
         n[i,a] <- sorted.data %>% select(N) %>% slice(line+a-1) %>% as.numeric()
         t[i,a] <- sorted.data %>% select(trt.jags) %>% slice(line+a-1) %>% as.numeric()
-        if (!is.null(meta.covariate)){x[i,a] <- sorted.data %>% select(covariate) %>% slice(line+a-1) %>% as.numeric()} #fix
+        if (!is.null(covariate)){x[i,a] <- sorted.data %>% select(covariate) %>% slice(line+a-1) %>% as.numeric()} #fix
       }
       line <- line + na[i]
     }
     
-    if (!is.null(meta.covariate)) {x <- x-mean(x, na.rm=TRUE)}
+    if (!is.null(covariate)) {x <- x-mean(x, na.rm=TRUE)}
     
     bugsdata2 <- list(ns=ns,
                       nt=nt,
@@ -184,12 +184,12 @@ nma.model <- function(data.nma,
         sd[i,a]<- sorted.data %>% select(sd) %>% slice(line+a-1) %>% as.numeric()
         n[i,a] <- sorted.data %>% select(N) %>% slice(line+a-1) %>% as.numeric()
         t[i,a] <- sorted.data %>% select(trt.jags) %>% slice(line+a-1) %>% as.numeric()
-        if (!is.null(meta.covariate)){x[i,a] <- sorted.data %>% select(covariate) %>% slice(line+a-1) %>% as.numeric()}        
+        if (!is.null(covariate)){x[i,a] <- sorted.data %>% select(covariate) %>% slice(line+a-1) %>% as.numeric()}        
       }
       line <- line + na[i]
     }
     se <- sd/sqrt(n)
-    if (!is.null(meta.covariate)) {x <- x-mean(x, na.rm=TRUE)}
+    if (!is.null(covariate)) {x <- x-mean(x, na.rm=TRUE)}
     
     bugsdata2 <- list(ns=ns,
                       nt=nt,
@@ -214,11 +214,11 @@ nma.model <- function(data.nma,
         E[i,a] <- sorted.data %>% select(timevar) %>% slice(line+a-1) %>% as.numeric()
         r[i,a] <- sorted.data %>% select(r1) %>% slice(line+a-1) %>% as.numeric()
         t[i,a] <- sorted.data %>% select(trt.jags) %>% slice(line+a-1) %>% as.numeric()
-        if (!is.null(meta.covariate)){x[i,a] <- sorted.data %>% select(covariate) %>% slice(line+a-1) %>% as.numeric()}
+        if (!is.null(covariate)){x[i,a] <- sorted.data %>% select(covariate) %>% slice(line+a-1) %>% as.numeric()}
       }
       line <- line + na[i]
     }
-    if (!is.null(meta.covariate)) {x <- x-mean(x, na.rm=TRUE)}
+    if (!is.null(covariate)) {x <- x-mean(x, na.rm=TRUE)}
     
     bugsdata2 <- list(ns=ns,
                       nt=nt,
@@ -230,7 +230,7 @@ nma.model <- function(data.nma,
                       na=na,
                       x=x) 
   } else if (family == "binomial" && link == "cloglog"){
-    time <- matrix(NA, ns, max(na))
+    time.mat <- matrix(NA, ns, max(na))
     n <- matrix(NA, ns, max(na))
     r <- matrix(NA, ns, max(na))
     t <- matrix(NA, ns, max(na))
@@ -240,21 +240,21 @@ nma.model <- function(data.nma,
     
     for(i in 1:ns){
       for(a in 1:na[i]){
-        time[i,a] <- sorted.data %>% select(timevar) %>% slice(line+a-1) %>% as.numeric()
+        time.mat[i,a] <- sorted.data %>% select(timevar) %>% slice(line+a-1) %>% as.numeric()
         n[i,a] <- sorted.data %>% select(N) %>% slice(line+a-1) %>% as.numeric()
         r[i,a] <- sorted.data %>% select(r1) %>% slice(line+a-1) %>% as.numeric()
         t[i,a] <- sorted.data %>% select(trt.jags) %>% slice(line+a-1) %>% as.numeric()
-        if (!is.null(meta.covariate)){x[i,a] <- sorted.data %>% select(covariate) %>% slice(line+a-1) %>% as.numeric()}        
+        if (!is.null(covariate)){x[i,a] <- sorted.data %>% select(covariate) %>% slice(line+a-1) %>% as.numeric()}        
       }
       line <- line + na[i]
     }
-    if (!is.null(meta.covariate)) {x <- x-mean(x, na.rm=TRUE)}
+    if (!is.null(covariate)) {x <- x-mean(x, na.rm=TRUE)}
     
     bugsdata2 <- list(ns=ns,
                       nt=nt,
                       #meanA=0,
                       #precA=0.5,
-                      time=time,
+                      "time"=time.mat,
                       n=n,
                       r=r,
                       t=t,
@@ -272,7 +272,7 @@ nma.model <- function(data.nma,
   ###Priors###
   ############
   
-  max.delta <- paste0(compute.prior(data.nma=data.nma, outcome=outcome, scale=scale, N=N, sd=sd, time = exposure.time))
+  max.delta <- paste0(compute.prior(data, outcome=outcome, scale=scale, N=N, sd=sd, time = time))
   
   # BASELINE EFFECTS PRIOR
   if (prior.mu == "DEFAULT"){
@@ -287,7 +287,7 @@ nma.model <- function(data.nma,
   
   # RELATIVE EFFECTS PRIOR
   if (prior.d =="DEFAULT"){
-    if(is.null(type)){
+    if(type=="consistency"){
       prior.d.str <- sprintf("for(k in 2:nt){
                              d[k] ~ dnorm(0,(%s*15)^(-2))
     }", max.delta)
@@ -300,7 +300,7 @@ nma.model <- function(data.nma,
     }", max.delta)
 		}
   } else {
-    if(is.null(type)){
+    if(type=="consistency"){
       prior.d.str <- sprintf("for(k in 2:nt){
                              d[k] ~ %s)
     }", prior.d)
@@ -332,7 +332,7 @@ nma.model <- function(data.nma,
    }
 
   #meta regression string
-  if (!is.null(meta.covariate)){
+  if (!is.null(covariate)){
     prior.meta.reg <- "beta[1]<-0
     for (k in 2:nt){
       beta[k] <- B
@@ -343,37 +343,36 @@ nma.model <- function(data.nma,
   }
   
   #remove covariate from bugsdata2 if unused
-  if (is.null(meta.covariate)){bugsdata2 <- bugsdata2[names(bugsdata2)!="x"]}
+  if (is.null(covariate)){bugsdata2 <- bugsdata2[names(bugsdata2)!="x"]}
   
 
   
-  if(!is.null(type)){
+  
     if(type=="inconsistency"){     
-      model.str <- makeBUGScode(family=family,
+      model <- makeBUGScode(family=family,
                                 link=link,
                                 effects=effects,
                                 inconsistency=TRUE,
                                 prior.mu.str,
                                 prior.d.str,
                                 prior.sigma2.str,
-                                meta.covariate,
+                                covariate,
                                 prior.meta.reg) %>%
         paste0(add.to.model)
-    }
-  } else if(is.null(type)){
-    model.str <- makeBUGScode(family=family, 
+    } else if(type=="consistency"){
+    model <- makeBUGScode(family=family, 
                               link=link, 
                               effects=effects, 
                               inconsistency=FALSE, 
                               prior.mu.str,
                               prior.d.str, 
                               prior.sigma2.str, 
-                              meta.covariate, 
+                              covariate, 
                               prior.meta.reg) %>%
       paste0(add.to.model)
   }
   
-  return(bugs=list(model.str=model.str,
+  return(bugs=list(model=model,
                    bugsdata2=bugsdata2, 
                    scale=scale, 
                    trt.map.table=trt.map.table, 
