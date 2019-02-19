3f156e53c9c12e11e5b72552c1125d2019c9f724
audrey-b <audrey476@hotmail.com>
2019-02-18 22:58:56 -0500

    data.plot streamlined

--- R/data.plot.R
+++ R/data.plot.R
@@ -2,11 +2,9 @@
 #' @description Plots a particular patient characteristic by study or by treatment. Useful for assessing
 #' differences in potential effect modifiers.
 #' 
-#' @param patients.data A dataset containing patient level data. \code{patients.data = my.slr$patient.data} is a
+#' @param data A dataset containing patient level data. \code{patients.data = my.slr$patient.data} is a
 #' convenient way to input this parameter
-#' @param treatment.var A string indicating the name of the treatment variable
-#' @param trial.var A string indicating the name of the study variable
-#' @param var.name A string indicating the name of the patient characteristic to be plotted
+#' @param covariate A string indicating the name of the patient characteristic to be plotted
 #' @param fill.str An optional string indicating the variable to categorize measurements. For instance,
 #' some studies report the mean treatment effect and others may report the median treatment effect. If there 
 #' is a variable in \code{patients.data} called "type.measure" indicating whether the mean or median is 
@@ -17,7 +15,7 @@
 #' error bar (see Example 2).
 #' @param errorbar.max Mandatory if errorbar=TRUE. A string indicating how to calculate the maximum of the 
 #' error bar (see Example 2).
-#' @param overall.avg If true, adds overall average line to plot.
+#' @param avg.hline If true, adds overall average line to plot.
 #' @param y.lab y-label of the plot.
 #' @param caption Caption of plot.
 #' @param by If by="trial" then data from arms will be grouped by study/trial. If by="treatment" then
@@ -26,10 +24,8 @@
 #' @examples
 #' # Example containing a fill.str, an overall average, and no error
 #' 
-#'data.plot(patients.data = my.slr$patient.data, 
-#'              treatment.var = "trt",
-#'              trial.var = "studyName",
-#'              var.name = "Age", 
+#'data.plot(data = my.slr,
+#'              covariate = "Age", 
 #'              fill.str="type_measure",
 #'              y.lab = "Age (Years)", 
 #'              caption = "Johnson study did not report age",
@@ -37,30 +33,39 @@
 #'              
 #'# Example containing no fill.str, no overall average, but contains errorbars
 #'
-#'data.plot(patients.data = my.slr$patient.data, 
-#'              treatment.var = "trt",
-#'              trial.var = "studyName",
-#'              var.name = "Age", 
+#'data.plot(data = my.slr,
+#'              covariate = "Age", 
 #'              errorbar = TRUE,
 #'              errorbar.min = "Age - Age_sd", 
 #'              errorbar.max = "Age + Age_sd",
-#'              overall.avg=FALSE,
+#'              avg.hline=FALSE,
 #'              y.lab = "Age (Years)", 
 #'              caption = "Error bars: Mean +/- sd",
 #'              by = "trial")
 
-data.plot <- function(patients.data, 
-                          treatment.var,
-                          trial.var,
-                          var.name, 
-                          errorbar = FALSE,
-                          errorbar.min = NULL, 
-                          errorbar.max = NULL,
-                          overall.avg=TRUE,
-                          fill.str=NULL,
-                          y.lab, 
-                          caption,
-                          by = "trial"){
+data.plot <- function(data,
+                          covariate, 
+                          half.length = NULL,
+                      by = "trial",
+                          avg.hline=TRUE,
+                          fill.str=NULL){
+  
+  patients.data <- data$arm.data
+  treatment.var <- data$varname.t
+  trial.var <- data$varname.s
+  y.lab <- covariate
+  if(is.null(half.length)){
+    errorbar = FALSE
+    errorbar.min = NULL
+    errorbar.max = NULL
+    caption <- ""
+  } else{
+    errorbar = TRUE
+    errorbar.min <- paste0(covariate,"-",half.length)
+    errorbar.max <- paste0(covariate,"+",half.length)
+    caption <- paste0("Error bars: ", covariate," +/- ", half.length)
+  }
+  
   
   if (errorbar == TRUE && (is.null(errorbar.min) | is.null(errorbar.max))){
     stop('When errorbar = TRUE, errorbar.min and errorbar.max must be specified')
@@ -69,8 +74,8 @@ data.plot <- function(patients.data,
   if (errorbar){eb = geom_errorbar(aes_string(ymin=errorbar.min, ymax=errorbar.max))}
   else {eb = NULL}
   
-  if (overall.avg){
-    overall.mean <- patients.data %>% select(var.name)%>%colMeans( na.rm=TRUE) %>% as.numeric()
+  if (avg.hline){
+    overall.mean <- patients.data %>% select(covariate)%>%colMeans( na.rm=TRUE) %>% as.numeric()
     om <- geom_hline(yintercept = overall.mean, color = "red", linetype=2)}
   else{om=NULL}
   
@@ -78,8 +83,8 @@ data.plot <- function(patients.data,
     mutate(trt =(!! as.name(treatment.var)), trial =(!! as.name(trial.var)))
   
   if (by == "trial"){
-  if(is.null(fill.str)) {p <- ggplot(patients.data, aes_string(x=treatment.var, y=var.name))}
-  else p <- ggplot(patients.data, aes_string(x=treatment.var, y=var.name, fill=fill.str))
+  if(is.null(fill.str)) {p <- ggplot(patients.data, aes_string(x=treatment.var, y=covariate))}
+  else p <- ggplot(patients.data, aes_string(x=treatment.var, y=covariate, fill=fill.str))
   
   p <- p + geom_bar(stat = "identity") +
     eb +
@@ -94,8 +99,8 @@ data.plot <- function(patients.data,
   }
   else if (by == "treatment"){
     
-  if(is.null(fill.str)) {p <- ggplot(patients.data, aes_string(x=trial.var, y=var.name))}
-  else p <- ggplot(patients.data, aes_string(x=trial.var, y=var.name, fill=fill.str))
+  if(is.null(fill.str)) {p <- ggplot(patients.data, aes_string(x=trial.var, y=covariate))}
+  else p <- ggplot(patients.data, aes_string(x=trial.var, y=covariate, fill=fill.str))
   
   p <- p + geom_bar(stat = "identity") +
     eb +
--- R/data.prep.R
+++ R/data.prep.R
@@ -2,16 +2,11 @@
 #' Data preparation
 #' @description Puts data into appropriate form for subsequent analysis using BUGSnet.
 #' @param arm.data Data with 1 row for each study arm.
-#' @param patient.data An optional separate data object containing patient characteristics (e.g comorbidities).
-#' Data should be in same format as \code{arm.data} (1 row for each arm). Patient characteristic data should 
-#' be plotted and compared when assessing the feasibility of an NMA.
-#'
 #' @param varname.t A string indicating the name of the treatment variable.
 #' @param varname.s A string indicating the name of the study variable.
 #' @param N A string indicating the name of the variable containing the number of participants in each arm
 #' 
 #' @return \code{arm.data} - A tibble containing the arm level study data
-#' @return \code{patient.data} - A tibble containing the arm level patient characteristics (if applicable)
 #' @return \code{treatments} - A list of all treatments in the network
 #' @return \code{studies} - A list of all studies in the network
 #' @return \code{n.arms} - A tibble containing the number of arms for each study
@@ -22,20 +17,15 @@
 #' # Example 1, no patient data
 #' my.data <- read.csv("diabetes_NMAdata.csv")
 #' diabetes.slr <- data.prep(arm.data = my.data, varname.t = "treatment", varname.s = "studyName", N="n_patients")
-#' 
-#' # Example 2, contains patient data
-#' NMA.data <- read.csv("MI_NMAdata.csv")
-#' patient.data <- read.csv("patient_covariates.csv")
-#' myocardInf.slr <- data.prep(arm.data = NMA.data, varname.t = "trt", varname.s = "stud", N="N")
 
 data.prep <- function(arm.data,
-                      patient.data = NULL,
+                      #patient.data = NULL,
                       varname.t,
                       varname.s,
                       N
 ){
   return(list(arm.data=arm.data,
-              patient.data = patient.data,
+              #patient.data = patient.data,
               treatments=arm.data %>% select_(varname.t) %>% unique %>% arrange_(varname.t),
               studies=arm.data %>% select_(varname.s) %>% unique %>% arrange_(varname.s),
               n.arms= arm.data %>% group_by_(varname.s) %>% summarize(n.arms = n()) %>% ungroup() %>% arrange,
