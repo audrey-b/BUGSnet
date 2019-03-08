636c5fffcde044390ba76af026821c5741e603d6
audrey-b <audrey476@hotmail.com>
2019-03-08 13:25:51 -0500

    add a message when loading

---
+++ R/xxx.R
@@ -0,0 +1,9 @@
+.onAttach <- function(libname, pkgname) {
+  
+  packageStartupMessage("This version of BUGSnet is provided to you under a\n",
+                        "Creative Commons Attribution-NonCommercial-ShareAlike\n",
+                        "International license (CC BY-NC-SA). You are responsible\n",
+                        "for conforming to the terms of this license. Commercial use\n",
+                        "of BUGSnet or any parts of BUGSnet is stricly prohibited\n",
+                        "under this license.")
+}
