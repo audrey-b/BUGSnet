9271e1859e30dac0c5ca176658da07bb8f9e3a7e
Justin Slater <40896411+JustinJSlater@users.noreply.github.com>
2019-03-13 18:18:42 -0400

    Updated trace plots to have different lines for each chain. Added 2 options, thin and trace.colors. Updated documentation to include these parameters, slightly modified example.

--- R/nma.trace.R
+++ R/nma.trace.R
@@ -2,6 +2,8 @@
 #' @description Produces traceplots of the MCMC chains obtained from \code{nma.run()}
 #' @param nma An output produced by \code{nma.run()}
 #' @param n Integer which limits the number of printed variables to the first \code{n}. Default is "all" which plots every variable.
+#' @param thin Thinning factor for the mcmc chains. Default is 1.
+#' @param trace.colors An optional vector of colors, 1 for each chain. 
 #' @export
 #' @seealso \code{\link{nma.run}}
 #' 
@@ -30,11 +32,14 @@
 #'n.burnin=1000,
 #'n.iter=10000)
 #'
-#'nma.trace(diabetes.re.c.res, n=4)
+#'nma.trace(diabetes.re.c.res, n=4, thin=10)
 #'  
 
-nma.trace <- function(nma, n="all"){
+nma.trace <- function(nma, n="all", thin = 1, trace.colors = "DEFAULT"){
   samples <- do.call(rbind, nma$samples) %>% data.frame()
+  
+  n.iter <- nrow(nma$samples[[1]]) 
+  n.chains <- nrow(samples)/n.iter
   
   # samples.vars <- colnames(samples)
   # scalars <- vars[which(vars %in% samples.vars)]
@@ -52,10 +57,31 @@ nma.trace <- function(nma, n="all"){
       num_plots = n
   }
 
+  if (trace.colors=="DEFAULT"){
+    colors=c("black", "red", "blue", "green", "purple", "yellow", "pink", "grey")#colors to be used in trace.plots
+  } else {
+    colors = trace.colors
+  }
+  
+  if(length(colors)<n.chains) stop("length(trace.colors) must be greater than n.chains.")
+  
   par(mfrow = c(num_plots, 2))
   for (i in 1:num_plots){
-    plot(samples[,i], type="l", main = paste(names(samples[i])))
+    plot(seq(thin, n.iter, thin), #ensures x-axis labels correspond to correct iteration
+         samples[seq(thin, n.iter, thin),i], 
+         type="l", 
+         main = paste(names(samples[i])), 
+         ylab="",
+         xlab="iteration",
+         col = colors[1],
+         ylim=c(min(samples[seq(thin, nrow(samples), thin),i]), 
+                max(samples[seq(thin, nrow(samples), thin),i]))) #ensure that each chain fits in plot
+    
+    if (n.chains>1){ 
+      for (j in 2:n.chains){
+          lines(seq(thin, n.iter, thin), samples[seq((j-1)*n.iter+thin, j*n.iter, thin), i ], col=colors[j])
+      }
+    }  
     plot(density(samples[,i]), main = paste(names(samples[i])))
   }
-   
 }
