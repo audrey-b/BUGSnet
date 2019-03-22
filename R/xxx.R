4d0c5c2ed852d7e7afd41c2348a5b640387d7a94
audrey-b <audrey476@hotmail.com>
2019-03-22 18:17:58 -0400

    citation and license message

--- R/xxx.R
+++ R/xxx.R
@@ -1,9 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   
-  packageStartupMessage("This version of BUGSnet is provided to you under a\n",
-                        "Creative Commons Attribution-NonCommercial-ShareAlike\n",
-                        "International license (CC BY-NC-SA). You are responsible\n",
-                        "for conforming to the terms of this license. Commercial use\n",
-                        "of BUGSnet or any parts of BUGSnet is stricly prohibited\n",
+  packageStartupMessage("/n This version of BUGSnet is distributed under the Creative Commons\n",
+                        "Attribution-NonCommercial-ShareAlike International license (CC BY-NC-SA 4.0).\n",
+                        "You are responsible for conforming to the terms of this license.\n",
+                        "Commercial use of BUGSnet or any parts of BUGSnet is stricly prohibited\n",
                         "under this license.")
 }
---
+++ inst/CITATION.R
@@ -0,0 +1,29 @@
+citHeader("To cite the BUGSnet package in publications use:")
+
+#' @Manual{,
+#'   title = {R: A Language and Environment for Statistical Computing},
+#'   author = {{R Development Core Team}},
+#'   organization = {R Foundation for Statistical Computing},
+#'   address = {Vienna, Austria},
+#'   year = {2008},
+#'   note = {{ISBN} 3-900051-07-0},
+#'   url = {http://www.R-project.org},
+#' }
+
+citEntry(entry="Manual",
+         
+         title = "BUGSnet: Bayesian network meta-analysis in compliance with best practice guidelines",
+         
+         author = personList(as.person("Audrey Béliveau", "Justin Slater", "Devon Boyne")),
+         
+         year = 2019,
+         
+         note = "R package licensed under CC BY-NC-SA 4.0",
+         
+         textVersion = 
+           
+           paste("Béliveau A., Slater J and Boyne D. (2019)", 
+                 
+                 "BUGSnet: Bayesian network meta-analysis in compliance with best practice guidelines",
+                 
+                 "R package licensed under CC BY-NC-SA 4.0."))
