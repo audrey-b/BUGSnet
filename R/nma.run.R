0f36a2fb39ce7e9731e7d2a5207d222f8a4bc818
Eric Mackay <emackay@lighthouseoutcomes.com>
2019-08-09 18:06:50 -0400

    Adds 'inits' parameter to nma.run which allows the user to pass inits options directly to jags.model. "DEFAULT" inits option uses the R random seed in order to initialize the random seeds for the JAGS chains. Resolves #56

--- R/nma.run.R
+++ R/nma.run.R
@@ -10,6 +10,9 @@
 #' @param n.iter Number of iterations for the mcmc chains.
 #' @param thin Thinning factor for the mcmc chains. Default is 1.
 #' @param n.chains Number of mcmc chains. Default is 3.
+#' @param inits Specifies initial values and random number generator options for each chain. The "DEFAULT" option uses the R random seed to set the JAGS random
+#' seeds. Non-default options are passed directly to \code{\link{jags.model}}. In order to use the JAGS default initialization, set inits to NULL. 
+#' See \code{\link{jags.model}} for more info.
 #' 
 #' @return \code{model} - The object obtained from \code{nma.model} that was used to run \code{jags}.
 #' @return \code{data} - The data used with the BUGS code.
@@ -18,7 +21,7 @@
 #' @return \code{family} - Family that was used for the NMA model (e.g normal, binomial, poisson)
 #' @return \code{link} - Link function that was used for the NMA model (e.g normal, binomial, poisson)
 #' @export
-#' @seealso \code{\link{nma.model}}, \code{\link{nma.fit}}, \code{\link{nma.league}}, \code{\link{nma.rank}}, \code{\link{nma.forest}}, \code{\link{nma.regplot}}, \code{\link{nma.trace}}
+#' @seealso \code{\link{nma.model}}, \code{\link{nma.fit}}, \code{\link{nma.league}}, \code{\link{nma.rank}}, \code{\link{nma.forest}}, \code{\link{nma.regplot}}, \code{\link{nma.trace}}, \code{\link{jags.model}}
 #' @examples
 #' data(diabetes.sim)
 #' 
@@ -53,12 +56,25 @@ nma.run <- function(model,
                     n.burnin = floor(n.iter / 2), 
                     n.iter, 
                     thin=1,
-                    n.chains=3){
+                    n.chains=3,
+                    inits = "DEFAULT"){
+  
+  if (!is.null(inits) && inits == "DEFAULT")
+  {
+    seed <- sample(.Machine$integer.max, 1)
+    seeds <- seed:(seed + min(.Machine$integer.max - seed, n.chains - 1))
+    if (length(seeds) < n.chains)
+      seeds <- c(seeds, 1:(n.chains - length(seeds)))
+    inits <- list()
+    for (i in 1:n.chains)
+      inits[[i]] <- list(.RNG.seed = seeds[i], .RNG.name = "base::Mersenne-Twister")
+  }
   
   jagsmodel <- jags.model(textConnection(model$bugs),
                           model$data,
                           n.chains=n.chains,
-                          n.adapt=n.adapt)
+                          n.adapt=n.adapt,
+                          inits = inits)
   
   if(n.burnin!=0) jagsburnin <- update(jagsmodel, n.iter=n.burnin)
   
