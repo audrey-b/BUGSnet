e0fdbc8fe9b0667af40d4a2d2b1fc2e90af494c1
audrey-b <audrey.beliveau@uwaterloo.ca>
2019-10-27 12:47:11 -0400

    added WHATSNEW and pdf manual

---
+++ WHATSNEW.md
@@ -0,0 +1,7 @@
+- corrected the warning messages due to an update to nest in tidyverse
+
+- updates to license
+
+- updates to citation("BUGSnet")
+
+- removed unused ... argument in net.plot to avoid specifying outcome = 
--- misc_code/build.package.R
+++ misc_code/build.package.R
@@ -1,37 +1,10 @@
-library(devtools)
-devtools::install(build_vignette = FALSE)
-library(BUGSnet)
-devtools::check()
+#change version
 
+#change WHATSNEW file
 
-devtools::load_all("R")
-document()
-devtools::check(document = FALSE)
-devtools::check()
-
+#build vignettes for website
 devtools::build_vignettes()
 
-#devtools::install(build_vignettes = TRUE)
-
-
-devtools::install()
-library(BUGSnet)
-
+#build pdf manual for website
 system("R CMD Rd2pdf . --title=Package BUGSnet --output=./manual.pdf --force --no-clean --internals")
-
-
-
-install.packages("~/BUGSnet_0.0.0.9000.tar.gz", repos = NULL, type = "source")
-library(BUGSnet)
-??BUGSnet
-
-
-
-
-devtools::document()
-devtools::build_vignettes()
-devtools::build()
-
-system("tar zxvf ../BUGSnet_0.0.0.9000.tar.gz")
-system("R CMD Rd2pdf BUGSnet")
-
+#delete .Rd2pdf8444 folder
